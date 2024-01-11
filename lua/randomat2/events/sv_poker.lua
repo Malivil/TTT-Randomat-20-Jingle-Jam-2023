38c6c7dca24707dea516b4074e2602d2ccf9d534
Logan <lobsterlogan43@yahoo.com>
2024-01-10 22:37:27 -0600

    Create files

---
+++ lua/randomat2/events/sv_poker.lua
@@ -0,0 +1,18 @@
+--// Logan Christianson
+
+local EVENT = {}
+
+EVENT.Title = "A Round Of Yogscast Poker"
+EVENT.Description = "You want the 9 of Diamonds"
+EVENT.id = "poker"
+-- EVENT.Type = EVENT_TYPE_WEAPON_OVERRIDE
+-- EVENT.Categories = {"rolechange", "entityspawn", "largeimpact"}
+
+function EVENT:Begin()
+end
+
+function EVENT:End()
+end
+
+function EVENT:GetConVars()
+end
