68b48677237459e6c515a259f02114599a2f6311
Logan <lobsterlogan43@yahoo.com>
2024-01-11 19:05:47 -0600

    Update sv_poker.lua

--- lua/randomat2/events/sv_poker.lua
+++ lua/randomat2/events/sv_poker.lua
@@ -3,16 +3,21 @@
 local EVENT = {}
 
 EVENT.Title = "A Round Of Yogscast Poker"
-EVENT.Description = "You want the 9 of Diamonds"
+EVENT.Description = "Only if the 9 of Diamonds touch!"
+EVENT.ExtDescription = "A round of 5-Card Draw Poker, bet with your health. Any pair, three, or four of a kind containing the 9 of Diamonds instantly wins."
 EVENT.id = "poker"
--- EVENT.Type = EVENT_TYPE_WEAPON_OVERRIDE
--- EVENT.Categories = {"rolechange", "entityspawn", "largeimpact"}
+EVENT.MinPlayers = 2
+EVENT.Type = EVENT_TYPE_DEFAULT
+EVENT.Categories = {"mediumimpact"} -- Add more
 
+-- Called when an event is started. Must be defined to for an event to work.
 function EVENT:Begin()
 end
 
+-- Called when an event is stopped. Used to do manual cleanup of processes started in the event.
 function EVENT:End()
 end
 
+-- Gets tables of the convars defined for an event. Used primarily by the Randomat 2.0 ULX module to dynamically create configuration pages for each event.
 function EVENT:GetConVars()
 end
