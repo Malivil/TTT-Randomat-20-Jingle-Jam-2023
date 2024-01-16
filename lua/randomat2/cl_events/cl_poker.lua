8de76c592c8734692ff8e6417d3fb991a0294410
Logan Christianson <lobsterlogan43@yahoo.com>
2024-01-16 15:53:58 -0600

    Start client work

--- lua/randomat2/cl_events/cl_poker.lua
+++ lua/randomat2/cl_events/cl_poker.lua
@@ -1,76 +1,131 @@
 --// Logan Christianson
 
-local Players = {}
+local EVENT = {}
+
+EVENT.Self = LocalPlayer()
+EVENT.Players = {}
+EVENT.IsPlaying = false
+
+function EVENT:SetupPanel()
+    -- Set up the base panel here
+    self.PanelActive = true
+
+    self.PokerMain = vgui.Create("DFrame", nil, "Poker Randomate Main Frame")
+    self.PokerMain:SetSize(100, 100)
+    self.PokerMain:SetPos(ScrW() - self.PokerMain:GetWide(), 100)
+    self.PokerMain:SetTitle("")
+	self.PokerMain:SetVisible(true)
+	self.PokerMain:SetDraggable(false)
+	self.PokerMain:ShowCloseButton(false)
+
+end
+
+function EVENT:ClosePanel()
+    -- Close base panel
+    self.PanelActive = false
+    self.PokerMain:Close()
+end
+
+function EVENT:RegisterPlayers(newPlayersTbl, selfIsIncluded)
+    self.IsPlaying = selfIsIncluded
+    self.Players = newPlayersTbl
+
+    if self.IsPlaying and not self.PanelActive then
+        self:SetupPanel()
+    end
+end
 
 net.Receive("StartPokerRandomat", function()
-    local numPlayers = net.ReadUInt(3)
+    local players = []
+    local selfIsPlaying = false
 
+    local numPlayers = net.ReadUInt(3)
     for i = 1, numPlayers do
-        table.insert(Players, net.ReadEntity())
+        local ply = net.ReadEntity()
+
+        table.insert(players, ply)
+
+        if ply == EVENT.Self then
+            selfIsPlaying = true
+        end
     end
 
-    if table.KeyFromValue(LocalPlayer()) then
+    EVENT:RegisterPlayers(players, selfIsPlaying)
+
+    if selfIsPlaying then
         net.Start("StartPokerRandomatCallback")
         net.SendToServer()
     end
 end)
 
-net.Receive("StartPokerRandomatCallback", function()
-
-end)
-
 net.Receive("NotifyBlinds", function()
+    if not EVENT.IsPlaying then return end
 
+    local smallBlind = net.ReadEntity()
+    local bigBlind = net.ReadEntity()
 end)
 
 net.Receive("DealCards", function()
+    if not EVENT.IsPlaying then return end
 
 end)
 
 net.Receive("StartBetting", function()
+    if not EVENT.IsPlaying then return end
 
 end)
 
 net.Receive("MakeBet", function()
+    if not EVENT.IsPlaying then return end
 
 end)
 
 net.Receive("PlayerFolded", function()
+    if not EVENT.IsPlaying then return end
 
 end)
 
 net.Receive("PlayerChecked", function()
+    if not EVENT.IsPlaying then return end
 
 end)
 
 net.Receive("PlayerCalled", function()
+    if not EVENT.IsPlaying then return end
 
 end)
 
 net.Receive("PlayerRaised", function()
+    if not EVENT.IsPlaying then return end
 
 end)
 
 net.Receive("StartDiscard", function()
+    if not EVENT.IsPlaying then return end
 
 end)
 
 net.Receive("PlayersFinishedBetting", function()
+    if not EVENT.IsPlaying then return end
 
 end)
 
 net.Receive("MakeDiscard", function()
+    if not EVENT.IsPlaying then return end
 
 end)
 
 net.Receive("RevealHands", function()
+    if not EVENT.IsPlaying then return end
 
 end)
 
 net.Receive("DeclareWinner", function()
+    if not EVENT.IsPlaying then return end
 
 end)
 
 net.Receive("ClosePokerWindow", function()
+    if not EVENT.IsPlaying then return end
 
 end)
