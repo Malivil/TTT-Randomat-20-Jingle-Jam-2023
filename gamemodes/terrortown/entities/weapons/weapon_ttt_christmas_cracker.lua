c63be6d7ede7531b78b37357e25f1fa684739967
The Stig <thestig294@gmail.com>
2024-04-01 22:37:41 +1030

    Added finding a cracker-opening partner, making you both slow, and forcibly making them face you

--- gamemodes/terrortown/entities/weapons/weapon_ttt_christmas_cracker.lua
+++ gamemodes/terrortown/entities/weapons/weapon_ttt_christmas_cracker.lua
@@ -16,7 +16,8 @@ SWEP.AutoSpawnable = false
 SWEP.AllowDrop = true
 SWEP.CanBuy = nil
 -- Arbitrary weapon kind to not conflict with any other weapon
-SWEP.Kind = 31135
+local lastKind = 31135
+SWEP.Kind = lastKind
 SWEP.Primary.ClipSize = -1
 SWEP.Primary.DefaultClip = -1
 SWEP.Primary.Ammo = "none"
@@ -27,59 +28,139 @@ SWEP.UseHands = false
 -- SWEP.ViewModel = "models/ttt_randomat_jingle_jam_2023/cracker/cracker_cracked_short.mdl"
 SWEP.WorldModel = "models/ttt_randomat_jingle_jam_2023/cracker/cracker.mdl"
 SWEP.ViewModel = "models/ttt_randomat_jingle_jam_2023/cracker/cracker.mdl"
+-- How far away in source units you can try to get someone to 
+SWEP.Range = 100
+-- How forgiving to the user to find a player they are trying to click on
+-- (Higher number = more lag compensation, but less accuracy, might find the wrong player if another player is near)
+SWEP.PartnerSearchHitBoxSize = 50
 
--- Places a paper crown hat on someone's head
-local hatColours = {COLOR_WHITE, COLOR_BLACK, COLOR_BLUE, COLOR_RED, COLOR_GREEN}
-
-function SWEP:GiveHat(ply)
-    if CLIENT or not IsValid(ply) or IsValid(ply.hat) then return end
-    local model = "models/ttt_randomat_jingle_jam_2023/paper_crown/paper_crown.mdl"
-    local hat = ents.Create("ttt_hat_deerstalker")
-    if not IsValid(hat) then return end
-    local pos = ply:GetPos()
-    hat:SetPos(pos)
-    hat:SetAngles(ply:GetAngles())
-    hat:SetParent(ply)
-
-    -- Hat doesn't like being set a lot of the time, so attempt to create it twice
-    timer.Simple(0, function()
-        if IsValid(hat) then
-            hat:SetModel(model)
-        end
-    end)
-
-    timer.Simple(0.1, function()
-        if not IsValid(hat) then
-            hat = ents.Create("ttt_hat_deerstalker")
-            hat:SetPos(pos)
-            hat:SetAngles(ply:GetAngles())
-            hat:SetParent(ply)
-            hat:SetModel(model)
-            hat:Spawn()
-        else
-            hat:SetModel(model)
-        end
-    end)
-
-    ply.hat = hat
-    hat:Spawn()
-    hat:SetColor(hatColours[math.random(#hatColours)])
-end
-
-function SWEP:Initialize()
-end
-
-function SWEP:Deploy()
-    self:GiveHat(self:GetOwner())
-
-    return true
-end
+-- Colours the paper hat can be
+SWEP.HatColours = {COLOR_WHITE, COLOR_BLACK, COLOR_GREEN, COLOR_RED, COLOR_YELLOW, COLOR_BLUE, COLOR_PINK, COLOR_ORANGE}
 
 function SWEP:PrimaryAttack()
 end
 
-function SWEP:Holster()
-    return true
+function SWEP:SecondaryAttack()
+end
+
+function SWEP:Reload()
+end
+
+if SERVER then
+    -- Places a paper crown hat on someone's head
+    function SWEP:GiveHat(ply)
+        if not IsValid(ply) or IsValid(ply.hat) then return end
+        local model = "models/ttt_randomat_jingle_jam_2023/paper_crown/paper_crown.mdl"
+        local hat = ents.Create("ttt_hat_deerstalker")
+        if not IsValid(hat) then return end
+        local pos = ply:GetPos()
+        hat:SetPos(pos)
+        hat:SetAngles(ply:GetAngles())
+        hat:SetParent(ply)
+
+        -- Hat doesn't like being set a lot of the time, so attempt to create it twice
+        timer.Simple(0, function()
+            if IsValid(hat) then
+                hat:SetModel(model)
+            end
+        end)
+
+        timer.Simple(0.1, function()
+            if not IsValid(hat) then
+                hat = ents.Create("ttt_hat_deerstalker")
+                hat:SetPos(pos)
+                hat:SetAngles(ply:GetAngles())
+                hat:SetParent(ply)
+                hat:SetModel(model)
+                hat:Spawn()
+            else
+                hat:SetModel(model)
+            end
+        end)
+
+        ply.hat = hat
+        hat:Spawn()
+        hat:SetColor(self.HatColours[math.random(#hatColours)])
+    end
+
+    -- Returns an entity the owner is looking at, with a tolerance of a bounding box to search for a player in
+    function SWEP:GetTraceEntity()
+        local owner = self:GetOwner()
+        if not IsValid(owner) then return end
+        local startPos = owner:GetShootPos()
+        local endPos = startPos + (owner:GetAimVector() * self.Range)
+        local lowerBoxBound = Vector(-1, -1, -1) * self.PartnerSearchHitBoxSize
+        local upperBoxBound = Vector(1, 1, 1) * self.PartnerSearchHitBoxSize
+
+        local TraceResult = util.TraceHull({
+            start = startPos,
+            endpos = endPos,
+            filter = owner,
+            mask = MASK_SHOT_HULL,
+            mins = lowerBoxBound,
+            maxs = upperBoxBound
+        })
+
+        return TraceResult.Entity
+    end
+
+    -- Frees players from trying to open the cracker
+    function SWEP:ResetCrackerPartner()
+        local owner = self:GetOwner()
+        if not IsValid(owner) then return end
+        local partner = owner:GetNWEntity("ChristmasCrackerPartner")
+
+        if IsValid(partner) then
+            partner:SetLaggedMovementValue(1)
+            partner:SetNWEntity("ChristmasCrackerPartner", NULL)
+        end
+
+        owner:SetLaggedMovementValue(1)
+        owner:SetNWEntity("ChristmasCrackerPartner", NULL)
+    end
+
+    -- Opens the cracker and randomly chooses a winner, biased towards players that have lost more cracker-openings (gambler's fallacy)
+    function SWEP:OpenCracker()
+        -- TODO: Paper hat, random item, cheesy joke
+        self:ResetCrackerPartner()
+    end
+
+    -- Cracker-opening logic
+    function SWEP:Think()
+        local owner = self:GetOwner()
+        if not IsValid(owner) then return end
+
+        -- First check the player is looking at someone and is holding down the left mouse button
+        if owner:KeyDown(IN_ATTACK) then
+            -- Think hooks are predicted so we can use lag compensation to help, only run this while the user is left-clicking to not add too much overhead
+            if owner.LagCompensation then
+                owner:LagCompensation(true)
+            end
+
+            local partner = self:GetTraceEntity()
+            if not IsPlayer(partner) then return end
+            -- Set flags on the cracker-opening partners
+            owner:SetNWEntity("ChristmasCrackerPartner", partner)
+            partner:SetNWEntity("ChristmasCrackerPartner", owner)
+            -- Make the partner face the player
+            local partnerAim = owner:GetAimVector()
+            partnerAim.x = -partnerAim.x
+            partnerAim.y = -partnerAim.y
+            partnerAim.z = -partnerAim.z
+            partnerAim = partnerAim:Angle()
+            partner:SetEyeAngles(partnerAim)
+            -- Slow the players down
+            partner:SetLaggedMovementValue(0.2)
+            owner:SetLaggedMovementValue(0.2)
+
+            -- TODO: Make the other player have to start holding left-click to start opening the cracker
+            if owner.LagCompensation then
+                owner:LagCompensation(false)
+            end
+        else
+            self:ResetCrackerPartner()
+        end
+    end
 end
 
 -- This hacks in a viewmodel for the SWEP using its worldmodel, instead of using a proper separate v_ or c_ model (Not enough tutorials for this online...)
